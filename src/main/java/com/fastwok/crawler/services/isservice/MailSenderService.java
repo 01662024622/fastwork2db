5ad7a6c01ef31ca51e537302651eacf4cd80f79e
Vu Minh Thang (TMS) <thangvm@bagroup.vn>
2022-04-26 14:24:27 +0700

    thangvm

--- src/main/java/com/fastwok/crawler/services/isservice/MailSenderService.java
+++
@@ -1,7 +0,0 @@
-package com.fastwok.crawler.services.isservice;
-
-import javax.mail.MessagingException;
-
-public interface MailSenderService {
-    public void sendMail(String content) throws MessagingException;
-}
